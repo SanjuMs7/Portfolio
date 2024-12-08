e2ee2b7073cf4c0f1e52f94ab0c445f5871e7a17
SanjuMs <sanjumlp777@gmail.com>
2024-12-08 11:23:51 +0530

    projects updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Copyright from './copyright/copyright.jsx'
 import About from './about/about.jsx'
 import Skills from './skills/skills.jsx'
 import Contact from './contact/contact.jsx'
+import EdusityProjectPreview from './assets/project-preview.png';
+import DbmsProjectPreview from './assets/dbms-project.png'
 function App() {
   return (
     <>
@@ -19,8 +21,17 @@ function App() {
           <Skills/>
           <Title heading="Browse My Recent" subheading="Projects"/>
           <div className='projects-container'>
-              
-              <Project/>
+              <Project projectName="Edusity" 
+                        projectDescription="Edusity is a straightforward website crafted using React.js as Part of the learning process. It features clean design and user-friendly interfaces, showcasing essential information effectively while leveraging React's capabilities for a responsive and intuitive browsing experience."
+                        projectPreview={EdusityProjectPreview}
+                        gitRepoLink="https://github.com/SanjuMs7/Edusity"
+                        livePreviewLink="https://sanjums7.github.io/Edusity/"
+                        />
+              <Project projectName="Mess Attendance" 
+                        projectDescription="The Mess Attendance App is a DBMS project collaboratively developed using MERN Stack to simplify attendance management with a clean interface and essential functionality. My contribution focused on designing and implementing the frontend UI, ensuring a responsive and engaging user experience by leveraging React's capabilities."
+                        projectPreview={DbmsProjectPreview}
+                        gitRepoLink=""
+                        livePreviewLink="https://mecmh.vercel.app/"/>
           </div>
           <Title heading="Get in Touch" subheading="Connect Me"></Title>
           <Contact/>
--- src/projects/project.jsx
+++ src/projects/project.jsx
@@ -1,12 +1,13 @@
 import './project.css';
-import projectPreview from '../assets/project-preview.png';
+import projectGithubRepo from '../assets/git-icon.png'
+import livePreviewIcon from '../assets/plus-icon.png'
 import React, { useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-function Project() {
+function Project({projectName,projectDescription,projectPreview,gitRepoLink,livePreviewLink}) {
   useEffect(() => {
     gsap.to(".project", {
       scrollTrigger: {
@@ -27,19 +28,13 @@ function Project() {
         <div className="project-image-preview-div">
           <img src={projectPreview} alt="Project Preview" />
           <div className="hidden-div">
-            <span className='heading'>Edusity</span>
+            <span className='heading'>{projectName}</span>
             <p className="project-description">
-              Edusity is a straightforward website crafted using React.js. It features clean design and user-friendly interfaces, showcasing essential information effectively while leveraging React's capabilities for a responsive and intuitive browsing experience.
+              {projectDescription}
             </p>
             <div className="project-button-div">
-              <a href="https://github.com/SanjuMs7/Edusity" target="_blank" rel="noopener noreferrer">
-                <button className="project-btn">Github</button>
-              </a>
-              <a href="https://sanjums7.github.io/Edusity/" target="_blank" rel="noopener noreferrer">
-                <button className="project-btn" style={{ backgroundColor: "#2a2a2a", color: "white" }}>
-                  Live Preview
-                </button>
-              </a>
+              <a href={gitRepoLink} target='_blank'><img src={projectGithubRepo} alt=''/></a>
+              <a href={livePreviewLink} target='_blank'><img src={livePreviewIcon} alt="" /></a>
             </div> 
         </div>
         </div>
