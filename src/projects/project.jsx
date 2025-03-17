60de4bd9dd75a9296dd63f81bd54cf3a7d746977
SanjuMs <sanjumlp777@gmail.com>
2025-03-17 21:25:05 +0530

    scaling done in skills section

--- src/projects/project.jsx
+++ src/projects/project.jsx
@@ -33,8 +33,8 @@ function Project({projectName,projectDescription,projectPreview,gitRepoLink,live
               {projectDescription}
             </p>
             <div className="project-button-div">
-              <a href={gitRepoLink}><img src={projectGithubRepo} alt=''/></a>
-              <a href={livePreviewLink} target='_blank'><img src={livePreviewIcon} alt="" /></a>
+              {/* <a href={gitRepoLink}><img src={projectGithubRepo} alt=''/></a>
+              <a href={livePreviewLink} target='_blank'><img src={livePreviewIcon} alt="" /></a> */}
             </div> 
         </div>
         </div>
